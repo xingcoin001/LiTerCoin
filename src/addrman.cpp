74d40356df05187a08cc286e22577f4572b8d17b
xingcoin001 <1713001283@qq.com>
2016-03-07 16:06:34 +0800

    Update addrman.cpp

--- src/addrman.cpp
+++ src/addrman.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2012 Pieter Wuille
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-//help
+//helps
 
 #include "addrman.h"
 
